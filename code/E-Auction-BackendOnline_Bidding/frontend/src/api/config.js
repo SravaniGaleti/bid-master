a0cd57c41f1bc9cba0226cde558e47b77be55e9a
SravaniGaleti <sravanigaleti05@gmail.com>
2024-10-25 00:27:49 +0530

    Delete code directory

--- code/E-Auction-BackendOnline_Bidding/frontend/src/api/config.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const baseURL = "http://192.168.1.85:5000/api/";
-export const imageURL = "http://192.168.1.85:5000";
-const axiosapi = axios.create({
-  baseURL,
-});
-export default axiosapi;
